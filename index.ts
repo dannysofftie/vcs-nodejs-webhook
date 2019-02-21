5889878d7b7b2f442fd6fc21c26be6f6aacc5044
Danny Sofftie <dankim761@gmail.com>
2019-02-21 14:34:41 +0300

    Add default get response

--- index.ts
+++ index.ts
@@ -4,6 +4,10 @@ import { exec } from 'child_process';
 import * as http from 'http';
 
 http.createServer(function(req, res) {
+    if (req.method == 'GET') {
+        res.end(JSON.stringify({ message: 'close' }));
+    }
+
     req.on('data', function(chunk) {
         exec('cd ' + repo + ' && git pull');
     });
