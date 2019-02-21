dffe928d15e2fe56103924df321bada1efab4b48
Danny Sofftie <dankim761@gmail.com>
2019-02-21 14:14:39 +0300

    Set up intial configs

---
+++ index.ts
@@ -0,0 +1,23 @@
+const secret = 'your_secret_here';
+const repo = '~/root/app/platform-server';
+
+const http = require('http');
+const crypto = require('crypto');
+const exec = require('child_process').exec;
+
+http.createServer(function(req, res) {
+    req.on('data', function(chunk) {
+        let sig =
+            'sha1=' +
+            crypto
+                .createHmac('sha1', secret)
+                .update(chunk.toString())
+                .digest('hex');
+
+        if (req.headers['x-hub-signature'] == sig) {
+            exec('cd ' + repo + ' && git pull');
+        }
+    });
+
+    res.end();
+}).listen(8080);
